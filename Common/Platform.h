0f0d093668c59799820e8f251fce46396b66252c
tghaefli <haefliger.p@gmx.net>
2015-10-09 14:35:37 +0200

    Revert "added the key files"
    This reverts commit 8619c0a2f577c1979890c173479a2d7d56ee4675.
    
    Conflicts:
    	Common/Keys.h
    	Common/Platform.h

--- Common/Platform.h
+++ Common/Platform.h
@@ -8,6 +8,7 @@
 #ifndef SOURCES_COMMON_PLATFORM_H_
 #define SOURCES_COMMON_PLATFORM_H_
 
+<<<<<<< HEAD
 #include "PE_Types.h"
 #include "PE_Const.h"
 #include "PE_Error.h"
@@ -33,6 +34,14 @@
 
 #define PL CONFIG HAS TIMER 1
 #endif
+=======
+#define K22FXROBO 1
+#define KL25ZFRDM 2
+
+#define PL_TARGETR_BOARD K22FXROBO
+
+
+>>>>>>> parent of 8619c0a... added the key files
 
 
 void PL_Init(void);
